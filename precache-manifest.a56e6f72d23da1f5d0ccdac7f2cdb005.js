1251503fc679a174bebd08571a09c5b6d4531d31
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-01-26 14:34:59 +0200

    Updates

---
+++ precache-manifest.a56e6f72d23da1f5d0ccdac7f2cdb005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8276aaaba0b33c4099eb1acd9ed5e71b",
+    "url": "/react-tr-business-card/index.html"
+  },
+  {
+    "revision": "8027bcb35bae84058c97",
+    "url": "/react-tr-business-card/static/css/main.561a96d6.chunk.css"
+  },
+  {
+    "revision": "c4d4aee7a768f7a7de06",
+    "url": "/react-tr-business-card/static/js/2.fea5a980.chunk.js"
+  },
+  {
+    "revision": "8027bcb35bae84058c97",
+    "url": "/react-tr-business-card/static/js/main.9e93be72.chunk.js"
+  },
+  {
+    "revision": "8c04d6d6ac8a1a785022",
+    "url": "/react-tr-business-card/static/js/runtime~main.8d8f9566.js"
+  },
+  {
+    "revision": "a700732020a77438182f68093c4f48db",
+    "url": "/react-tr-business-card/static/media/card.a7007320.jpg"
+  }
+]);
